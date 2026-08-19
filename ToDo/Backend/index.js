e083c341991f1cace4497915cd43af58202df541
Lucas Eduardo Pereira da Cruz <lucascruzjau@gmail.com>
2026-08-19 09:36:04 -0300

    Adicionando formulário (arrumar erro)

--- ToDo/Backend/index.js
+++ ToDo/Backend/index.js
@@ -14,7 +14,7 @@ const app = new express();
 app.use(express.json());
 app.use(cors({
     credentials: true, 
-    origin: "http://localhost:3000"
+    origin: "http://localhost:5173"
 })); 
 
 // Obrigatoriamente o swagger deve vir antes das rotas 
