b59c9de97b811a186bc3c3316c1ba80d022b5f15
Lucas Eduardo Pereira da Cruz <lucascruzjau@gmail.com>
2026-09-17 09:29:22 -0300

    Criando o sistema de voz dentro do projeto

---
+++ ToDo/Frontend/src/Hook/useVoiceRecognition.js
@@ -0,0 +1,114 @@
+import {useState, useEffect, useRef} from "react"; 
+// Função tratar conversões data falada 
+function interpretarDataVoz(texto){
+    const fala = texto.toLowerCase().trim(); 
+    const hoje = new Date()
+    if(fala.includes("hoje")){
+        return hoje.toISOString().split("T")[0] //Pega só indice 0, que é somente a data
+    }
+    if(fala.includes("amanhã") || fala.includes("amanha")){
+        const amanha = new Date(); 
+        amanha.setDate(hoje.getDate()+1); 
+        return amanha.toISOString().split("T")[0]; 
+    }
+    if(fala.includes("depois de amanhã") || fala.includes("depois de amanhã")){
+        const depoisAmanha = new Date(); 
+        depoisAmanha.setDate(hoje.getDate()+2); 
+        return depoisAmanha.toISOString().split("T")[0]; 
+    }
+    const matchDias = fala.match(/daqui a (\d+) dias/); 
+    if(matchDias){
+        const dias = parseInt(matchDias[1], 10); 
+        const dataFutura = new Date();
+        dataFutura.setDate(hoje.getDate() + dias)
+        return depoisAmanha.toISOString().split("T")[0]; 
+    }
+    return ""; 
+} // Fim da função 
+
+export function useVoiceRecognition(){
+    const [textoOuvido, setTextoOuvido] = useState(""); 
+    const [ouvindo, setOuvindo] = useState("false"); 
+    const [suportado, setSuportado] = useState("true");
+    const recognitionRef = useRef(null) 
+    useEffect(()=>{
+        // Verifica se a API está disponível no navegador 
+        if(typeof window !== "undefined"){
+            const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+        }
+        if(SpeechRecognition){
+            const recognition = new SpeechRecognition(); 
+            //microfone ficar contínuo 
+            recognition.continuous = true; 
+            recognition.interimResults = true; 
+            recognition.lang = "pt-BR"
+            // Converte em texto após o áudio 
+            recognition.onresult = (event) => {
+                let transcricaoFinal = ""; 
+                // Acumula todos os trechos da fala confirmados durante a sessão ativa 
+                for(let i = event.resultIndex; i < event.result.length; i++){
+                    if(event.results[i].isFinal){
+                        transcricaoFinal += event.results[i][0].transcript + ""; 
+                    }
+                }
+                if(transcricaoFinal){
+                    setTextoOuvido(transcricaoFinal.trim()); 
+                }
+            };
+            // evento erro 
+            recognition.onerror = (event) => {
+                console.error("Erro no conhecimento de voz", event.error); 
+                setOuvindo(false); 
+            };
+            // fim da fala
+            recognition.onend = () =>{
+                setOuvindo(false); 
+            };
+            recognition.current = recognition; 
+        }
+        else
+        {
+            setSuportado(false); 
+        }
+    }, []); 
+    // Inicia ou interrompe a gravação (liga/desliga)
+    const iniciarEscuta = ()=>{
+        if(!recognitionRef.current) return; 
+        if(ouvindo){
+            // se já estiver ouvindo o click manual encerra a gravação 
+            recognitionRef.current.stop(); 
+            setOuvindo(false); 
+        }
+        else 
+        {
+            // limpa textos e inicia a escuta 
+            setTextoOuvido(""); 
+            setOuvindo(true)
+            recognitionRef.current.start(); 
+        }
+    }; 
+    // Função para parar gravação manualmente 
+    const pararEscuta = ()=>{
+        if(recognitionRef.current && ouvindo){
+            recognitionRef.current.stop(); 
+            setOuvindo(false); 
+        }
+    }; 
+    // Processar a frase capturada e atualiza o estado correspondente baseado na palavra-chave
+    const processarComandoVoz = (
+        fala, 
+        setTitulo,
+        setDescricao, 
+        setDataLimite, 
+        usuarios = [], 
+        handleCheckBoxChange)=>{
+            // expressões 
+            const regexTitulo = /(?:título|titulo)\s+(.+)/i;
+            const regexDescricao = /(?:descrição|descricao)\s+(.+)/i;
+            const regexData = /(?:data|data limite|prazo)\s+(.+)/i;
+            const regexParticipante = /(?:participante|participantes|adicionar|incluir)\s+(.+)/i;
+        };
+         
+
+    
+}
